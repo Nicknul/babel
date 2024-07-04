82b6b431affa001b464b7ae331be87a81d271355
Nicknul <jea7730@gmail.com>
2024-07-04 10:24:41 +0900

    :sparkles: add_정호연 : babel 사용
    1. .bebelrc 파일 안에 설치한 @babel/preset-typescript를 기재
    2. npx babel work.ts -o work.babel.js 명령을 통해 js 파일로 변환

---
+++ work.babel.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var add = function add(a, b) {
+  var result = a + b;
+  return result;
+};
+var hoyeon = function hoyeon() {
+  var obj = {
+    name: '정호연',
+    age: 27,
+    job: '학생'
+  };
+  return obj;
+};
